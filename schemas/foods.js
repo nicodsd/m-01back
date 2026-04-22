16a59aac690e451fb9b1214dfdde12c15740289c
nicodsd <nilonb7@gmail.com>
2026-04-22 17:13:12 -0300

    schema

--- schemas/foods.js
+++ schemas/foods.js
@@ -1,12 +1,31 @@
 import Joi from "joi-oid";
 
 export const foodSchema = Joi.object({
-  name: Joi.string().required(),
-  photo: Joi.string().uri().required(), // Cloudinary debe haber puesto esto ya
-  description: Joi.string().min(10).required(),
-  price: Joi.number().required(),
-  category: Joi.string().allow(null, ""),
-  sub_category: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "string.empty": "Faltó el nombre del plato",
+    "string.required": "Faltó el nombre del plato",
+  }),
+  photo: Joi.string().uri().required().messages({
+    "string.empty": "Faltó la foto",
+    "any.invalid": "URL invalid",
+    "string.uri": "URL invalid",
+  }),
+  description: Joi.string().min(5).max(25).required().messages({
+    "string.required": "Faltó la descripción del plato",
+    "string.min": "La descripción del plato es muy corta",
+    "string.max": "La descripción del plato es muy larga",
+  }),
+  price: Joi.number().required().max(1000000).messages({
+    "string.empty": "Faltó el precio",
+    "number.required": "Faltó el precio",
+    "number.max": "El precio es muy alto",
+  }),
+  category: Joi.string().allow(null, "").messages({
+    "string.empty": "Faltó la categoría",
+  }),
+  sub_category: Joi.string().required().messages({
+    "string.empty": "Faltó la sub categoría",
+  }),
   promo_price: Joi.number().allow(0).optional(),
   is_promo: Joi.boolean().optional(),
   order: Joi.number().allow(null, 0).optional(),
@@ -17,22 +36,18 @@ export const foodSchemaUpdate = Joi.object({
     "string.empty": "Faltó el nombre del plato",
   }),
   photo: Joi.string().uri().optional().messages({
-    "string.empty": "Faltó la foto",
-    "any.invalid": "URL invalid",
+    "any.invalid": "Imagen inválida",
+    "string.uri": "Imagen inválida",
   }),
-  description: Joi.string().min(5).optional().messages({
-    "string.required": "Faltó la descripción del plato",
+  description: Joi.string().min(5).max(25).optional().messages({
     "string.min": "La descripción del plato es muy corta",
+    "string.max": "La descripción del plato es muy larga",
   }),
-  price: Joi.number().optional().messages({
-    "string.empty": "Faltó el precio",
+  price: Joi.number().optional().max(1000000).messages({
+    "number.max": "El precio es muy alto",
   }),
-  category: Joi.string().optional().messages({
-    "string.empty": "Faltó la categoría",
-  }),
-  sub_category: Joi.string().optional().messages({
-    "string.empty": "Faltó la sub categoría",
-  }),
+  category: Joi.string().optional(),
+  sub_category: Joi.string().optional(),
   promo_price: Joi.number().allow(0).optional(),
   is_promo: Joi.boolean().optional(),
   order: Joi.number().allow(null, 0).optional(),
