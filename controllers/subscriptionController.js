1e638d50b35717e68b1717dea53d9f478644191e
Nico Barrera <57334167+nicodsd@users.noreply.github.com>
2026-06-12 22:47:11 -0300

    Modify subscription link for user registration
    Update subscription link to point to user registration.

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -3,7 +3,7 @@ import { MercadoPagoConfig, PreApprovalPlan } from 'mercadopago';
 const client = new MercadoPagoConfig({
     accessToken: process.env.MP_ACCESS_TOKEN
 });
-const Link = `${process.env.APP_URL}/registro-de-usuario/3/success`
+const Link = `${process.env.APP_URL}/registro-de-usuario`
 
 export const createSubscription = async (req, res) => {
     try {
