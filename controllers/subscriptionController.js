4054feb231b7a8e37b8a628e23e85eb7eaeada4f
Nico Barrera <57334167+nicodsd@users.noreply.github.com>
2026-06-12 22:14:23 -0300

    Update back_url to use only APP_URL

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -23,7 +23,7 @@ export const createSubscription = async (req, res) => {
 
         const result = await subscription.create({
             body: {
-                back_url: `${process.env.APP_URL}/registro-de-usuario/3/success`,
+                back_url: process.env.APP_URL,
                 reason: reason,
                 auto_recurring: {
                     frequency: 1,
